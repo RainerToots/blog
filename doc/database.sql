7031c050383272e24382cd830eeb0ce0c4f33ddb
RainerToots <rainer.toots@khk.ee>
2018-04-23 13:28:46 +0300

    database.sql uuendus

--- doc/database.sql
+++ doc/database.sql
@@ -3,22 +3,22 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 26, 2018 at 10:16 AM
--- Server version: 5.6.39
--- PHP Version: 7.0.27-0+deb9u1
+-- Loomise aeg: Aprill 23, 2018 kell 01:28 PM
+-- Serveri versioon: 5.7.21
+-- PHP versioon: 7.0.27-0+deb9u1
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `blog`
+-- Andmebaas: `blog`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `post`
+-- Tabeli struktuur tabelile `post`
 --
 
 DROP TABLE IF EXISTS `post`;
@@ -31,16 +31,16 @@ CREATE TABLE `post` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `post`
+-- Andmete tõmmistamine tabelile `post`
 --
 
 INSERT INTO `post` (`post_id`, `post_subject`, `post_text`, `post_created`, `user_id`) VALUES
-(1, 'Hello World!', 'Test Test ', '2018-03-26 14:13:52', 1);
+  (1, 'Hello World!', 'Test Test ', '2018-03-26 14:13:52', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `translations`
+-- Tabeli struktuur tabelile `translations`
 --
 
 DROP TABLE IF EXISTS `translations`;
@@ -54,40 +54,40 @@ CREATE TABLE `translations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `translations`
+-- Andmete tõmmistamine tabelile `translations`
 --
 
 INSERT INTO `translations` (`translation_id`, `phrase`, `language`, `translation`, `controller`, `action`) VALUES
-(1, 'Action', 'en', '{untranslated}', 'welcome', 'index'),
-(2, 'Server returned response in an unexpected format', 'en', '{untranslated}', 'global', 'global'),
-(3, 'Forbidden', 'en', '{untranslated}', 'global', 'global'),
-(4, 'Server returned an error', 'en', '{untranslated}', 'global', 'global'),
-(5, 'Action', 'en', '{untranslated}', 'halo', 'index'),
-(6, 'Logout', 'en', '{untranslated}', 'global', 'global'),
-(7, 'Settings', 'en', '{untranslated}', 'global', 'global'),
-(8, 'Logged in as', 'en', '{untranslated}', 'global', 'global'),
-(9, 'Log out', 'en', '{untranslated}', 'global', 'global'),
-(10, 'Action', 'et', '{untranslated}', 'welcome', 'index'),
-(11, 'Log out', 'et', '{untranslated}', 'global', 'global'),
-(12, 'Server returned response in an unexpected format', 'et', '{untranslated}', 'global', 'global'),
-(13, 'Forbidden', 'et', '{untranslated}', 'global', 'global'),
-(14, 'Server returned an error', 'et', '{untranslated}', 'global', 'global'),
-(15, 'Please sign in', 'et', '{untranslated}', 'global', 'global'),
-(16, 'Email', 'et', '{untranslated}', 'global', 'global'),
-(17, 'Password', 'et', '{untranslated}', 'global', 'global'),
-(18, 'Sign in', 'et', '{untranslated}', 'global', 'global'),
-(19, 'Please sign in', 'en', '{untranslated}', 'global', 'global'),
-(20, 'Email', 'en', '{untranslated}', 'global', 'global'),
-(21, 'Password', 'en', '{untranslated}', 'global', 'global'),
-(22, 'Sign in', 'en', '{untranslated}', 'global', 'global'),
-(23, 'Oops...', 'en', '{untranslated}', 'global', 'global'),
-(24, 'Close', 'en', '{untranslated}', 'global', 'global'),
-(25, 'Server returned an error. Please try again later ', 'en', '{untranslated}', 'global', 'global');
+  (1, 'Action', 'en', '{untranslated}', 'welcome', 'index'),
+  (2, 'Server returned response in an unexpected format', 'en', '{untranslated}', 'global', 'global'),
+  (3, 'Forbidden', 'en', '{untranslated}', 'global', 'global'),
+  (4, 'Server returned an error', 'en', '{untranslated}', 'global', 'global'),
+  (5, 'Action', 'en', '{untranslated}', 'halo', 'index'),
+  (6, 'Logout', 'en', '{untranslated}', 'global', 'global'),
+  (7, 'Settings', 'en', '{untranslated}', 'global', 'global'),
+  (8, 'Logged in as', 'en', '{untranslated}', 'global', 'global'),
+  (9, 'Log out', 'en', '{untranslated}', 'global', 'global'),
+  (10, 'Action', 'et', '{untranslated}', 'welcome', 'index'),
+  (11, 'Log out', 'et', '{untranslated}', 'global', 'global'),
+  (12, 'Server returned response in an unexpected format', 'et', '{untranslated}', 'global', 'global'),
+  (13, 'Forbidden', 'et', '{untranslated}', 'global', 'global'),
+  (14, 'Server returned an error', 'et', '{untranslated}', 'global', 'global'),
+  (15, 'Please sign in', 'et', '{untranslated}', 'global', 'global'),
+  (16, 'Email', 'et', '{untranslated}', 'global', 'global'),
+  (17, 'Password', 'et', '{untranslated}', 'global', 'global'),
+  (18, 'Sign in', 'et', '{untranslated}', 'global', 'global'),
+  (19, 'Please sign in', 'en', '{untranslated}', 'global', 'global'),
+  (20, 'Email', 'en', '{untranslated}', 'global', 'global'),
+  (21, 'Password', 'en', '{untranslated}', 'global', 'global'),
+  (22, 'Sign in', 'en', '{untranslated}', 'global', 'global'),
+  (23, 'Oops...', 'en', '{untranslated}', 'global', 'global'),
+  (24, 'Close', 'en', '{untranslated}', 'global', 'global'),
+  (25, 'Server returned an error. Please try again later ', 'en', '{untranslated}', 'global', 'global');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Tabeli struktuur tabelile `users`
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -101,61 +101,61 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Andmete tõmmistamine tabelile `users`
 --
 
 INSERT INTO `users` (`user_id`, `is_admin`, `password`, `email`, `deleted`, `name`) VALUES
-(1, 1, '$2y$10$vTje.ndUFKHyuotY99iYkO.2aHJUgOsy2x0RMXP1UmrTe6CQsKbtm', 'demo@example.com', 0, 'Demo User');
+  (1, 1, '$2y$10$vTje.ndUFKHyuotY99iYkO.2aHJUgOsy2x0RMXP1UmrTe6CQsKbtm', 'demo@example.com', 0, 'Demo User');
 
 --
--- Indexes for dumped tables
+-- Indeksid tõmmistatud tabelitele
 --
 
 --
--- Indexes for table `post`
+-- Indeksid tabelile `post`
 --
 ALTER TABLE `post`
   ADD PRIMARY KEY (`post_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `translations`
+-- Indeksid tabelile `translations`
 --
 ALTER TABLE `translations`
   ADD PRIMARY KEY (`translation_id`),
   ADD UNIQUE KEY `language_phrase_controller_action_index` (`language`,`phrase`,`controller`,`action`);
 
 --
--- Indexes for table `users`
+-- Indeksid tabelile `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT tõmmistatud tabelitele
 --
 
 --
--- AUTO_INCREMENT for table `post`
+-- AUTO_INCREMENT tabelile `post`
 --
 ALTER TABLE `post`
   MODIFY `post_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `translations`
+-- AUTO_INCREMENT tabelile `translations`
 --
 ALTER TABLE `translations`
   MODIFY `translation_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT tabelile `users`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- Constraints for dumped tables
+-- Tõmmistatud tabelite piirangud
 --
 
 --
--- Constraints for table `post`
+-- Piirangud tabelile `post`
 --
 ALTER TABLE `post`
   ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
